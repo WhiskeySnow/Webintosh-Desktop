0c3c420735d163b9e886651d427e09ef5c178e57
WhiskeySnow <yawen.deng07@gmail.com>
2026-09-23 20:48:24 -0400

    Add src/javascripts/story.js

---
+++ src/javascripts/story.js
@@ -0,0 +1,39 @@
+import "./english-ui.js";
+
+import { create } from "./window.js";
+import { createNotification } from "./ui/notification.js";
+import { appMenu } from "./finderbar.js";
+
+window.choiceStory = window.choiceStory || {
+    installed: false,
+    choiceStep: 0,
+    messageMode: "mia",
+    miaStep: 0,
+    deleting: false,
+    choiceDesktopItem: null,
+    screen: "welcome"
+};
+
+appMenu["信息"] = ["File", "Edit", "View", "Window", "Help"];
+appMenu["CHOICE"] = ["File", "Edit", "View", "Window", "Help"];
+
+function startChoiceStory() {
+    setTimeout(() => {
+        createNotification(
+            "./assets/icons/信息.svg",
+            "Mia",
+            "you need to try this lol",
+            () => {
+                window.choiceStory.messageMode = "mia";
+                create("./assets/apps/信息.html", "信息", null, true);
+            },
+            "Now"
+        );
+    }, 1200);
+}
+
+if (document.readyState === "complete") {
+    startChoiceStory();
+} else {
+    window.addEventListener("load", startChoiceStory, { once: true });
+}
